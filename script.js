ecc63cccf1d9ed1c6b4bbcaa2d43aa1f3e6317b4
Navneet Rajput <108276935+NavneetRajput1@users.noreply.github.com>
2023-01-18 19:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+const len = str.length;
+	for (let i = 0; i < len / 2; i++) {
+if (str[i]!==st[len-1-i]) {
+	return true;
+}
+		return false;
 }
 module.exports = palindrome
